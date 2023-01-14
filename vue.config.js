93dd3bae4894d3e97af352039de2753b61d241e5
Adrian <hodurbalder@gmail.com>
2023-01-13 20:33:44 -0600

    add

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: 'https://crud-users-back.vercel.app',
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'  ? '' : ''
 })
+
