d56b6e6a72d89b52035cf5c916c283a296c0a038
Adrian <hodurbalder@gmail.com>
2023-01-13 21:08:30 -0600

    add

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default()=> {
     return axios.create({
-        baseURL: 'https://crud-users-front.vercel.app',
+        baseURL: 'https://crud-users-front.vercel.app:8080',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
